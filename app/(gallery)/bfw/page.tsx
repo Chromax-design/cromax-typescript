2ec5369a8c2a77d50cdd99a302c25aff1490e09c
Chromax-design <chromax.design@gmail.com>
2025-04-10 19:29:12 +0100

    updated the funfest and bfw pages

---
+++ app/(gallery)/bfw/page.tsx
@@ -0,0 +1,12 @@
+import TitleComponent from '@/components/TitleComponent'
+import React from 'react'
+
+const Bfw = () => {
+    return (
+        <main className=' my-14 space-y-5 p-3'>
+            <TitleComponent title={'BFW gallery'} />
+        </main>
+    )
+}
+
+export default Bfw
---
+++ app/(gallery)/funfest/page.tsx
@@ -0,0 +1,12 @@
+import TitleComponent from '@/components/TitleComponent'
+import React from 'react'
+
+const page = () => {
+    return (
+        <main className=' my-14 space-y-5 p-3'>
+            <TitleComponent title={'funfest gallery'} />
+        </main>
+    )
+}
+
+export default page
