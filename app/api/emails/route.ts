21d9e520556008b63fc385b1be7c57cf89eba69c
Chromax-design <chromax.design@gmail.com>
2025-04-11 23:46:19 +0100

    corrected

--- app/api/emails/route.ts
+++ app/api/emails/route.ts
@@ -5,21 +5,6 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendResponse = async (firstName: string, email: string) => {
-  try {
-    const data = await resend.emails.send({
-      from: "Cromax Photography <onboarding@resend.dev>",
-      to: email,
-      subject: 'Thanks for your message',
-      react: SendResponseTemplate(firstName)
-    });
-    return NextResponse.json({ success: true, data });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ success: false, error });
-  }
-}
-
 export async function POST(req: Request) {
   try {
     const body = await req.json();
@@ -32,7 +17,6 @@ export async function POST(req: Request) {
       subject: subject,
       react: SendMailTemplate(emailData)
     });
-    await sendResponse(firstName, email);
     return NextResponse.json({ success: true, data });
   } catch (error) {
     console.error(error);
